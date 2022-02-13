4dc9911689c13e23c00fcd0f6064a482d5ef2467
HorinouchiRyuichi <75193456+pro-channel@users.noreply.github.com>
2022-02-14 06:42:39 +0900

    #03 propsとuseStateの説明動画で使用したコードです

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,27 +1,43 @@
 import './App.css';
-import { Fragment } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const text = "click";
+  const [number, setNumber] = useState(0);
   const onClickButton = () => {
-    alert(text);
+    setNumber(number + 1);
   }
+
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <>
     {console.log("RUN!")}
     <div className='hello'>
       <h1>Hello world</h1>
     </div>
-    <p>はじめまして</p>
-    <button onClick={onClickButton}>{text}</button>
-    <Hello toWhat="japan"/>
-    <MonthDay month="7" day="10" />
+
+    <hr />
+    {<p>{number}</p>}
+    <button onClick={onClickButton}>count up</button>
+
+    <hr />
+    <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+    <button onClick={() => alert(inputValue)}>value check</button>
+    
+    <hr />
+    <TitleDay title="新しいfunctionを追加しました">
+      <YearMonthDay year="2022" month="7">15(Fri)</YearMonthDay>
+    </TitleDay>
     </>
   );
 }
 
-function MonthDay(props) {
-  return <div>{props.month}月 {props.day}日</div>
+function TitleDay({title, children}) {
+  return <h1>{title} : {children}</h1>
+}
+
+function YearMonthDay(props) {
+  return <span>{props.year}/{props.month}/{props.children}</span>
 }
 
 function Hello(props) {
